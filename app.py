bf3724da329315574972f30f6b88969c28644ba6
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2021-12-31 01:05:08 +0200

    update artist by id

--- app.py
+++ app.py
@@ -356,28 +356,54 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
-  return render_template('forms/edit_artist.html', form=form, artist=artist)
+  try:
+    artist = Artist.query.get(artist_id)
+    # if artist exist init form data with artist
+    form = ArtistForm(name=artist.name,
+                      city=artist.city, 
+                      state=artist.state,
+                      phone=artist.phone,
+                      image_link=artist.image_link,
+                      genres=artist.genres,
+                      facebook_link=artist.facebook_link,
+                      website_link=artist.website,
+                      seeking_venue=artist.seeking_venue,
+                      seeking_description=artist.seeking_description)
+    # TODO: populate form with fields from artist with ID <artist_id>
+    return render_template('forms/edit_artist.html', form=form, artist=artist)
+  except:
+    abort(404)
 
+
+  
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
+  try:
+    artist = Artist.query.get(artist_id)
+    form = ArtistForm(request.form)
+    if form.validate():
+      artist.name = form.name.data
+      artist.city = form.city.data
+      artist.state = form.state.data
+      artist.phone = form.phone.data
+      artist.genres = form.genres.data
+      artist.image_link = form.image_link.data
+      artist.facebook_link = form.facebook_link.data
+      artist.website = form.website_link.data
+      artist.seeking_venue = form.seeking_venue.data
+      artist.seeking_description = form.seeking_description.data
 
+      db.session.add(artist)
+      db.session.commit()
+      flash("Artist updated successfully!")
+      return redirect(url_for('show_artist', artist_id=artist_id))
+    else:
+      flash("Please check all data is correct!")
+      return render_template('forms/edit_artist.html', form=form, artist=artist)
+  except:
+    abort(404)
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
