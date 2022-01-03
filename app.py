d86eaa849a2b545d6b55a47b2ab604f1715aefa2
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2022-01-04 01:13:53 +0200

    get venue by id

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def delete_venue(venue_id):
     db.session.delete(current_venue)
     db.session.commit()
     # delete related shows
-    related_shows = Show.query.filter(Show.venue_id=venue_id).delete()
+    #related_shows = Show.query.filter(Show.venue_id=venue_id).delete()
   except:
     db.session.rollback()
     abort(500)
@@ -385,27 +385,28 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
+  try:
+    # get venue by id
+    venue = Venue.query.get(venue_id)
+    # initailize form with data
+    form = VenueForm(name=venue.name,
+                    city=venue.city, 
+                    state=venue.state,
+                    phone=venue.phone,
+                    address=venue.address,
+                    image_link=venue.image_link,
+                    genres=venue.genres,
+                    facebook_link=venue.facebook_link,
+                    website_link=venue.website,
+                    seeking_talent=venue.seeking_talent,
+                    seeking_description=venue.seeking_description)
+    return render_template('forms/edit_venue.html', form=form, venue=venue)
+  except:
+    abort(404)
+  
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
   return redirect(url_for('show_venue', venue_id=venue_id))
 
