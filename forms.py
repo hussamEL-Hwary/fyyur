9a5e37168c204c4f5da13cba392549a83ce665c3
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2022-01-07 22:36:27 +0200

    add validations

--- forms.py
+++ forms.py
@@ -191,7 +191,7 @@ class ArtistForm(Form):
     )
     phone = StringField(
         # TODO implement validation logic for state
-        'phone'
+        'phone', validators=[DataRequired()]
     )
     image_link = StringField(
         'image_link'
