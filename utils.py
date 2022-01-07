f0f83a7dc6c8ec97a69a765c589776587184ec4d
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2022-01-07 02:04:03 +0200

    separate helper methods in untils module

---
+++ utils.py
@@ -0,0 +1,12 @@
+import babel
+import dateutil.parser
+
+
+# format datetime
+def format_datetime(value, format='medium'):
+  date = dateutil.parser.parse(value)
+  if format == 'full':
+      format="EEEE MMMM, d, y 'at' h:mma"
+  elif format == 'medium':
+      format="EE MM, dd, y h:mma"
+  return babel.dates.format_datetime(date, format, locale='en')
